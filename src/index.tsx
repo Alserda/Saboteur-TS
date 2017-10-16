2f2a79ba506e04826244b26ecb3371253c390504
Peter Alserda <peteralserda@hotmail.com>
2017-10-16 23:32:22 +0200

    tsconfig updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-console.log('Index tsx');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import RootApp from './RootApp';
+
+const target = document.getElementById('app');
